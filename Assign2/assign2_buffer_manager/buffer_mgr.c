689c5d7372743aff5477c33b36ba7173b585621c
DeneshwaraSai <iladenesh99@gmail.com>
2024-02-29 18:52:14 -0600

    Added Assignment 2.

--- Assign2/assign2_buffer_manager/buffer_mgr.c
+++ Assign2/assign2_buffer_manager/buffer_mgr.c
@@ -24,6 +24,8 @@ int hitCount = 0;
 int pointerForClock = 0;
 int pointerForLFU = 0;
 
+void getConditionalBasedStrategy(BM_BufferPool *const bm);
+
 /*
 initBufferPool creates a new buffer pool with numPages page frames using the page replacement
 strategy strategy. The pool is used to cache pages from the page file with name pageFileName.
@@ -152,6 +154,188 @@ RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page) {
 
 RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
 		const PageNumber pageNum) {
-                
+
+        FrameInPage * frameInPage = (FrameInPage *)bm->mgmtData;
+
+        if(frameInPage[0].pageNumber == -1) {
+            SM_FileHandle fileHandler;
+
+            openPageFile(bm->pageFile, &fileHandler);
+            frameInPage[0].data = (SM_PageHandle) malloc (PAGE_SIZE);
+
+            ensureCapacity(pageNum, &fileHandler);
+
+            readBlock(pageNum, &fileHandler, frameInPage[0].data);
+
+            frameInPage[0].pageNumber=pageNum;
+            frameInPage[0].fixCountInfo++;
+            indexRear=hitCount=0;
+            frameInPage[0].hitNumber=hitCount;
+            frameInPage[0].referNumber=0;
+            page->pageNum=pageNum;
+            page->data = frameInPage[0].data;
+
+            return RC_OK;
+        } else {
+
+            bool isBufferFull = true;
+
+            for (int i=0;i<bufferSize;i++) {
+                if(frameInPage[i].pageNumber != -1) {
+                    if(frameInPage[i].pageNumber == pageNum) {
+
+                        frameInPage[i].fixCountInfo++;
+                        isBufferFull = false;
+
+                        if(bm->strategy == RS_CLOCK) {
+                            frameInPage[i].hitNumber = 1;
+                        }
+                        if (bm->strategy == RS_LRU) {
+                            frameInPage[i].hitNumber = hitCount;
+                        }
+                        if(bm->strategy == RS_LFU) {
+                            frameInPage[i].referNumber++;
+                        } 
+
+                        page->pageNum = pageNum;
+                        page->data = frameInPage[i].data;
+
+                        pointerForClock++;
+                        break;
+                    }
+                } else {
+                    SM_FileHandle fileHandler;
+
+                    openPageFile(bm->pageFile, &fileHandler);
+                    frameInPage[i].data = (SM_PageHandle) malloc (PAGE_SIZE);
+
+                    readBlock(pageNum, &fileHandler, frameInPage[i].data);
+                    
+                    indexRear++;
+                    hitCount++;
+
+                    frameInPage[i].referNumber=0;
+                    frameInPage[i].pageNumber=pageNum;
+                    frameInPage[i].fixCountInfo=1;
+
+                    if (bm->strategy == RS_CLOCK) {
+                        frameInPage[i].hitNumber = 1; 
+                    } else if(bm->strategy == RS_LRU) {
+                        frameInPage[i].hitNumber = hitCount;
+                    }
+
+                    page->pageNum = pageNum;
+                    page->data = frameInPage[i].data;
+
+                    isBufferFull = false;
+                    break;  
+                }
+            }
+
+            if (isBufferFull == true) {
+                FrameInPage * frameInPageTemp = (FrameInPage *) malloc (sizeof(FrameInPage));
+
+                SM_FileHandle fileHandler;
+
+                openPageFile(bm->pageFile, &fileHandler);
+
+                frameInPageTemp->data = (SM_PageHandle) malloc (PAGE_SIZE);
+
+                readBlock(pageNum, &fileHandler, frameInPageTemp->data);
+
+                frameInPageTemp->pageNumber = pageNum;
+                frameInPageTemp->fixCountInfo = 1;
+                frameInPageTemp->referNumber = 0;
+                frameInPageTemp->dirtyBit = 0;
+
+                indexRear++;
+                hitCount++;
+
+                if(bm->strategy == RS_LRU) {
+                    frameInPageTemp->hitNumber = hitCount;
+                }
+                else if (bm->strategy == RS_CLOCK) {
+                    frameInPageTemp->hitNumber = 1;
+                }
+
+                page->data = frameInPageTemp->data;
+                page->pageNum = pageNum;
+
+                getConditionalBasedStrategy(bm);
+
+            }
+        }
         return RC_OK;
     }
+
+
+void getConditionalBasedStrategy(BM_BufferPool *const bm) {
+
+    switch (bm->strategy) {
+        case RS_CLOCK:
+            break;
+
+        case RS_LRU:
+            break;
+
+        case RS_FIFO:
+            break;
+
+        case RS_LFU:
+            break;
+
+        case RS_LRU_K: 
+            break;
+
+        default:
+            printf("You are in in wrong algorithm. This Algorithm is not available.");
+            break;
+    }
+}
+
+
+/* ------------------ Statistics Interface ------------------ */
+
+PageNumber *getFrameContents (BM_BufferPool *const bm) {
+    int i = 0;
+    PageNumber * framesInPageNumber = malloc(sizeof(PageNumber) * bufferSize);
+    FrameInPage * frameInPage = (FrameInPage *) bm->mgmtData;
+
+    while (i< bufferSize) {
+        framesInPageNumber[i] = (frameInPage[i].pageNumber != -1) ? frameInPage[i].pageNumber : NO_PAGE;
+        i++;
+    }
+
+    return framesInPageNumber;
+}
+
+bool * getDirtyFlags (BM_BufferPool *const bm) {
+    FrameInPage * frameInPage = (FrameInPage *) bm->mgmtData;
+    bool *isFlagDirty = mallloc(sizeof(bool) * bufferSize);
+
+    for (int i=0;i<bufferSize;i++) {
+        isFlagDirty[i] = (frameInPage->dirtyBit == 1) ? true : false;
+    }
+
+    return isFlagDirty;
+}
+
+int *getFixCounts (BM_BufferPool *const bm) {
+    FrameInPage * frameInPage = (FrameInPage *) bm->mgmtData;
+    int *fixCounts = malloc(sizeof(int) * bufferSize);
+
+    for (int i=0;i<bufferSize;i++) {
+        fixCounts[i] = (frameInPage[i].fixCountInfo != -1) ? frameInPage[i].fixCountInfo : 0;
+    }
+    return fixCounts;
+}
+
+int getNumReadIO (BM_BufferPool *const bm) {
+    // The read IO number is present at rear index so returning that.
+    return (indexRear + 1);
+}
+
+int getNumWriteIO (BM_BufferPool *const bm) {
+    // Returning the write IO number by using writeCount.
+    return writeCount;
+}
